71c69d5283fd94f3dced8d8c0c4a5844649e7d65
decolua <decoluadt@gmail.com>
2014-08-02 23:28:10 +0700

    Add

---
+++ test/register.php
@@ -0,0 +1,8 @@
+<form action="" method="post">
+	<input name="email"/>
+	<input name="password"/>
+	<input name="name"/>
+	<input name="country"/>
+	<input name="birthday"/>
+	<input name="birthday"/>
+</form>
